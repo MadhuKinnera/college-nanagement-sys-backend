664f19b20aea013953a41fdd896206e9d08f8a1c
MadhuKinnera <hollypoter5@gmail.com>
2023-04-04 01:00:41 +0530

    backend added

---
+++ CollegeManagementforAjaySir/src/main/java/com/masai/CollegeManagementforAjaySirApplication.java
@@ -0,0 +1,17 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeManagementforAjaySirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeManagementforAjaySirApplication.class, args);
+		
+	}
+	
+	
+	
+
+}
